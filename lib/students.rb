40f3ac26b1ba703993cd27ae2e50841401584d19
Melissa Real <melissa.real@hotmail.com>
2018-08-24 16:15:52 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -8,10 +8,11 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  SELECT AVG(gpa) 
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
+  "SELECT COUNT(*)"
 end
 
 def average_gpa_for_9th_grade
